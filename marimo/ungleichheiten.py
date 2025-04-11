65f209870352456b9e31c9b32bf6943abd1b1a52
oaugele <oaugele@gmail.com>
2025-04-11 13:27:52 +0200

    marimo csv path fixed

--- marimo/ungleichheiten.py
+++ marimo/ungleichheiten.py
@@ -21,7 +21,7 @@ def _():
     import pandas as pd
 
     df = pd.read_csv(
-        "https://oaugele.github.io/ungleichheiten/data/ungleichheiten.csv",
+        "data/ungleichheiten.csv",
         header=0,
         delimiter=";",
     )
